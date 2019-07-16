458513e1b6fc264c667b46ac8c79f41f94d2068c
Wes Karle <wesleykarle@gmail.com>
2019-07-16 09:28:00 -0700

    sync

--- Program.cs
+++ Program.cs
@@ -4,14 +4,31 @@ namespace Song
 {
     class Program
     {
+        static void SingASong()
+        {
+            SingASong("Baby Shark!");
+        }
         static void SingASong(string Title)
         {
-            Console.WriteLine($"Let's sing {Title}");
+            SingASong(Title, "C");
+        }
+        static void SingASong(string Title, string key)
+        {
+            SingASong(Title, key, 5);
+
+        }
+        static void SingASong(string Title, string key, int nrTimes)
+        {
+            for (int i = 0; i < nrTimes; i++)
+            {
+                Console.WriteLine($"Sing: {Title} (in {key})");
+            }
         }
         static void Main(string[] args)
         {
             Console.WriteLine("\n\tSong.Program.Main()");
-            SingASong("Baby Shark!");
+            SingASong("Get Low");
+            //SingASong("Happy Birthday", "B minor", 2);
         }
     }
 }
