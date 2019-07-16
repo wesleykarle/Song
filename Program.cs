bf1eaf35358930f2c3dcbf71f536d956f16b4b3a
Wes Karle <wesleykarle@gmail.com>
2019-07-16 09:42:42 -0700

    Updated using optional parameters and confusing overloads. Bad code example!

--- Program.cs
+++ Program.cs
@@ -4,6 +4,13 @@ namespace Song
 {
     class Program
     {
+        static void SingASong(string Title= "Norwegian Wood", string key = "D", int nrTimes = 3)
+        {
+            for (int i = 0; i < nrTimes; i++)
+            {
+                Console.WriteLine($"Sing {Title} in {key}");
+            }
+        }
         static void SingASong()
         {
             SingASong("Baby Shark!");
@@ -17,17 +24,18 @@ namespace Song
             SingASong(Title, key, 5);
 
         }
-        static void SingASong(string Title, string key, int nrTimes)
-        {
-            for (int i = 0; i < nrTimes; i++)
-            {
-                Console.WriteLine($"Sing: {Title} (in {key})");
-            }
-        }
+        //static void SingASong(string Title, string key, int nrTimes)
+        //{
+        //    for (int i = 0; i < nrTimes; i++)
+        //    {
+        //        Console.WriteLine($"Sing: {Title} (in {key})");
+        //    }
+        //}
         static void Main(string[] args)
         {
             Console.WriteLine("\n\tSong.Program.Main()");
-            SingASong("Get Low");
+            SingASong(Title: "Wheels on the Bus");
+            //SingASong("Get Low");
             //SingASong("Happy Birthday", "B minor", 2);
         }
     }
