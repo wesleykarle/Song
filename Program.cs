a7c7a0f2d0d18b6c06ebcdca391a19c00e20494e
Wes Karle <wesleykarle@gmail.com>
2019-07-16 09:25:56 -0700

    Added Method overloads.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Song
+{
+    class Program
+    {
+        static void SingASong(string Title)
+        {
+            Console.WriteLine($"Let's sing {Title}");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\tSong.Program.Main()");
+            SingASong("Baby Shark!");
+        }
+    }
+}
